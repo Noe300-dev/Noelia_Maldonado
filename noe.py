c398d94a6a8649c7c7bab345e4d6ec4c2e988975
Noe300-dev <noemaldonadoaguirre@gmail.com>
2025-04-15 13:19:11 -0400

    python primera clase

--- noe.py
+++ noe.py
@@ -1,17 +1,66 @@
-# # declaracion de variable 
+# # # declaracion de variable 
 
-# nombre="link"
-# edad=31
-# # ejemplo de concatenacion
-# print("hola",  nombre, "y su edad es", edad)
+# # nombre="link"
+# # edad=31
+# # # ejemplo de concatenacion
+# # print("hola",  nombre, "y su edad es", edad)
 
-# print("ingrese su nombre")
-# nombre=input()
-# print("hola",  nombre)
+# # print("ingrese su nombre")
+# # nombre=input()
+# # print("hola",  nombre)
 
-# print("ingrese 2 numeros")
+# # print("ingrese 2 numeros")
 
-n1=int(input("ingrese un numero"))
-n2=int(input("ingrese un numero"))
-print(n1+n2)
-print("el resultado de la suma es", n1+n2)
+# # n1=int(input("ingrese un numero"))
+# # n2=int(input("ingrese un numero"))
+# # print(n1+n2)
+# # print("el resultado de la suma es", n1+n2)
+
+
+
+# print ("ingrese 3 numeros para sacar su promedio")
+# n1=int(input("ingrese un numero")) 
+# n2=int(input("ingrese un numero"))
+# n3=int(input("ingrese un numero"))
+# prom=(n1+n2+n3)/3
+# print("el promedio es", prom)
+
+# if prom>=40:
+#     print("el alumno aprobó")
+# else:
+#     print ("el alumno reprobó") 
+
+
+# verificacion de mayoria de edad
+
+# edad=int(input("ingrese su edad"))
+# if edad>=18 and edad<=17:
+#     print("es mayor de edad")
+# else:
+#     print("es menor de edad")
+
+
+# niño menor de 12 
+# adolescente entre 12 y 17 
+# mayor de edad mas o igual a 18
+
+# edad=int(input("ingrese su edad"))
+# if edad<12:
+#     print("tiene", edad, "por tanto es un niño")
+# elif edad>=12 and edad<= 17:
+#     print("tiene", edad, "por tanto es adolescente")
+# else:
+#     print("tiene", edad, "por tanto es mayor de edad")
+
+# ingrese 3 numeros y muestre el mayo
+
+# n1=int(input("ingrese un numero"))
+# n2=int(input("ingrese otro numero"))
+# n3=int(input("ingrese otro numero"))
+# if n1>n1 and n1>n3:
+#     print("el numero", n1, "es mayor")
+# elif n2>n3:
+#     print("el numero ", n2, "es el mayor ")
+# else:
+#     print("el numero", n3, "es mayor")
+
