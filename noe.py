fb1f40a7dddd7acfc11c66537e08d4605d6713fc
Noe300-dev <noemaldonadoaguirre@gmail.com>
2025-04-15 11:54:26 -0400

    primer comit

---
+++ noe.py
@@ -0,0 +1,17 @@
+# # declaracion de variable 
+
+# nombre="link"
+# edad=31
+# # ejemplo de concatenacion
+# print("hola",  nombre, "y su edad es", edad)
+
+# print("ingrese su nombre")
+# nombre=input()
+# print("hola",  nombre)
+
+# print("ingrese 2 numeros")
+
+n1=int(input("ingrese un numero"))
+n2=int(input("ingrese un numero"))
+print(n1+n2)
+print("el resultado de la suma es", n1+n2)
